fb66f583379021fad259f1fdead7a420901803a3
richard <mirach.qiu@gmail.com>
2020-02-03 08:40:36 +0800

    add macos config

--- server/steps.sh
+++ server/steps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+apt update
+apt install python-pip -y
+pip install setuptools
+pip install shadowsocks
+sed -i 's/cleanup/reset/g' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 cp ssserver.service /etc/systemd/system/ssserver.service
 cp server.json /etc/shadowsocks/server.json
 systemctl start ssserver
