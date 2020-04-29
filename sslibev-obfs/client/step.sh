1bafe3952d456910b4071a4c37869c514ea15395
richard <mirach.qiu@gmail.com>
2020-04-29 19:52:00 +0800

    modify script

--- sslibev-obfs/client/step.sh
+++ sslibev-obfs/client/step.sh
@@ -1,7 +1,8 @@
 #brew update
 #brew install shadowsocks-libev
 #brew install simple-obfs
-sudo vi /usr/local/etc/shadowsocks-libev.json
+sudo vi shadowsocks-libev.json
+sudo cp shadowsocks-libev.json /usr/local/etc/shadowsocks-libev.json
 sudo cp com.shadowsocks.plist /Library/LaunchDaemons/
 sudo launchctl stop com.shadowsocks.plist
 sudo launchctl start com.shadowsocks.plist
--- sslibev-obfs/server/step.sh
+++ sslibev-obfs/server/step.sh
@@ -1,5 +1,6 @@
 apt install shadowsocks-libev
-vi /etc/shadowsocks-libev/config.json
+vi config.json
+cp config.json /etc/shadowsocks-libev/config.json
 apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libc-ares-dev libev-dev asciidoc xmlto automake git -y
 git clone https://github.com/shadowsocks/simple-obfs.git
 cd simple-obfs
